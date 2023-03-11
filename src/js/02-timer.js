ced9e0475a20fc03e958dc0d943408726d33ed24
Joanna K <joannakleinrm@gmail.com>
2023-03-11 16:54:34 +0100

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,3 +2,19 @@
 import flatpickr from "flatpickr";
 // Dodatkowy import stylów
 import "flatpickr/dist/flatpickr.min.css";
+
+const input = document.querySelector("#datetime-picker");
+// console.log(input);
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+    },
+};
+const fp = flatpickr(input, options);
+
+// fp.addEventListener("input", options);
