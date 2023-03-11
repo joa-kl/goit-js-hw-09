052eb9d217a746b0f3a3441f6664d71577951a95
Joanna K <joannakleinrm@gmail.com>
2023-03-11 21:46:35 +0100

    timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,9 +2,17 @@
 import flatpickr from "flatpickr";
 // Dodatkowy import stylów
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
 const input = document.querySelector("#datetime-picker");
-// console.log(input);
+const startBtn = document.querySelector("button[data-start]");
+const days = document.querySelector("span[data-days");
+const hours = document.querySelector("span[data-hours]")
+const minutes = document.querySelector("span[data-minutes]");
+const seconds = document.querySelector("span[data-seconds]");
+// const timerHtml = document.querySelector('.timer');
+
+startBtn.disabled = true;
 
 const options = {
     enableTime: true,
@@ -12,9 +20,54 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        console.log(selectedDates[0]);
-    },
+        if (selectedDates[0] < new Date()) {
+            Notiflix.Notify.failure("Please choose a date in the future");
+            startBtn.disabled = true;
+        } else {
+            startBtn.disabled = false;
+        }
+    }
 };
-const fp = flatpickr(input, options);
 
-// fp.addEventListener("input", options);
+flatpickr(input, options);
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(v) {
+    return v.toString().padStart(2, '0');
+}
+
+startBtn.addEventListener("click", () => {
+    let timer = setInterval(() => {
+        let countdown = new Date(input.value) - new Date();
+        startBtn.disabled = true;
+        if (countdown >= 0) {
+            let timeObject = convertMs(countdown);
+            days.textContent = addLeadingZero(timeObject.days);
+            hours.textContent = addLeadingZero(timeObject.hours);
+            minutes.textContent = addLeadingZero(timeObject.minutes);
+            seconds.textContent = addLeadingZero(timeObject.seconds);
+            // if (countdown <= 30000) {
+            //     timerHtml.style.color = 'tomato';
+            // }
+        }
+        
+    }, 1000);
+})
