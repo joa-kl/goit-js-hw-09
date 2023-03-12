24c77f5bf6f56e847286f747edb4ee36f30e9da8
Joanna K <joannakleinrm@gmail.com>
2023-03-12 18:53:46 +0100

    promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import { Notify } from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const form = document.querySelector(".form");
+form.addEventListener("submit", onSubmit);
+function onSubmit(event) {
+  event.preventDefault();
+  let delay = Number(event.currentTarget.delay.value);
+  const amount = Number(event.currentTarget.amount.value);
+  const step = Number(event.currentTarget.step.value);
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(onSucces)
+      .catch(onError);
+    delay += step;
   }
+}
+
+function onSucces({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+function onError({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
